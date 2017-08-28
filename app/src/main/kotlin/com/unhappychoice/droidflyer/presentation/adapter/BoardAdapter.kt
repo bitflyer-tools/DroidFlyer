a26527679642f26cb7a7415222c60c5e846d591f
Yuji Ueki <unhappychoice@gmail.com>
2017-08-28 17:52:54 +0900

    :sparkles: Show order board

---
+++ app/src/main/kotlin/com/unhappychoice/droidflyer/presentation/adapter/BoardAdapter.kt
@@ -0,0 +1,91 @@
+package com.unhappychoice.droidflyer.presentation.adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.RelativeLayout
+import android.widget.TextView
+import com.jakewharton.rxbinding2.view.clicks
+import com.unhappychoice.droidflyer.R
+import com.unhappychoice.droidflyer.extension.Variable
+import com.unhappychoice.droidflyer.extension.splitByComma
+import com.unhappychoice.droidflyer.infrastructure.bitflyer.model.Order
+import com.unhappychoice.droidflyer.presentation.style.DefaultStyle
+import com.unhappychoice.norimaki.extension.subscribeNext
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.subjects.PublishSubject
+
+class BoardAdapter(val context: Context, val type: BoardType): RecyclerView.Adapter<BoardAdapter.ViewHolder>() {
+    val items = Variable<List<Order>>(listOf())
+    val clickItems = PublishSubject.create<Order>()
+    private val bag = CompositeDisposable()
+
+    override fun getItemCount() = items.value.size
+
+    override fun onBindViewHolder(holder: BoardAdapter.ViewHolder, position: Int) {
+        val item = items.value[position]
+        holder.bind(item)
+        holder.view.clicks().subscribeNext { clickItems.onNext(item) }.addTo(bag)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): BoardAdapter.ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.order_list_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    inner class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(order: Order) {
+            priceTextView.text = order.price.splitByComma()
+            sizeTextView.text = "%.8f".format(order.size)
+
+            priceTextView.setTextColor(type.color())
+            sizeTextView.setTextColor(type.color())
+
+            priceTextView.alignParent(type.priceDirection())
+            sizeTextView.alignParent(type.sizeDirection())
+
+            val screenWidth = (context.getSystemService(Context.WINDOW_SERVICE) as WindowManager).defaultDisplay.width
+            val width = screenWidth / 2.0 * Math.min(Math.log10(order.size + 1) / 2.0, 1.0)
+            backgroundView.setBackgroundColor(type.color())
+            backgroundView.alignParent(type.sizeDirection(), width.toInt())
+        }
+
+        private fun View.alignParent(direction: Int, width: Int? = null) {
+            val params = RelativeLayout.LayoutParams(
+                width ?: RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+            )
+            params.addRule(direction)
+            params.alignWithParent = true
+            layoutParams = params
+        }
+
+        private val priceTextView = view.findViewById(R.id.price) as TextView
+        private val sizeTextView = view.findViewById(R.id.size) as TextView
+        private val backgroundView = view.findViewById(R.id.backgroundView)
+    }
+}
+
+sealed class BoardType {
+    abstract fun sizeDirection(): Int
+    abstract fun priceDirection(): Int
+    abstract fun color(): Int
+
+    object Ask: BoardType() {
+        override fun color() = DefaultStyle.sellColor
+        override fun priceDirection() = RelativeLayout.ALIGN_PARENT_RIGHT
+        override fun sizeDirection() = RelativeLayout.ALIGN_PARENT_LEFT
+    }
+
+    object Bid: BoardType() {
+        override fun color() = DefaultStyle.buyColor
+        override fun priceDirection() = RelativeLayout.ALIGN_PARENT_LEFT
+        override fun sizeDirection() = RelativeLayout.ALIGN_PARENT_RIGHT
+
+    }
+}
--- app/src/main/kotlin/com/unhappychoice/droidflyer/presentation/view/LimitOrderView.kt
+++ app/src/main/kotlin/com/unhappychoice/droidflyer/presentation/view/LimitOrderView.kt
@@ -1,26 +1,51 @@
 package com.unhappychoice.droidflyer.presentation.view
 
 import android.content.Context
+import android.support.v7.widget.LinearLayoutManager
 import android.util.AttributeSet
+import android.util.Log
 import com.github.salomonbrys.kodein.instance
+import com.unhappychoice.droidflyer.domain.service.CurrentStatusService
+import com.unhappychoice.droidflyer.presentation.adapter.BoardAdapter
+import com.unhappychoice.droidflyer.presentation.adapter.BoardType
 import com.unhappychoice.droidflyer.presentation.presenter.LimitOrderPresenter
+import com.unhappychoice.droidflyer.presentation.style.DefaultStyle
 import com.unhappychoice.droidflyer.presentation.view.core.BaseView
+import com.unhappychoice.norimaki.extension.subscribeNext
+import com.unhappychoice.norimaki.extension.subscribeOnIoObserveOnUI
+import io.reactivex.rxkotlin.addTo
+import kotlinx.android.synthetic.main.limit_order_view.view.*
+import java.util.concurrent.TimeUnit
 
-class LimitOrderView(context: Context?, attr: AttributeSet?) : BaseView(context, attr) {
+class LimitOrderView(context: Context, attr: AttributeSet?) : BaseView(context, attr) {
     val presenter: LimitOrderPresenter by instance()
+
+    private val currentStatusService: CurrentStatusService by instance()
+    private val askAdapter by lazy { BoardAdapter(context, BoardType.Ask) }
+    private val bidAdapter by lazy { BoardAdapter(context, BoardType.Bid) }
 
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
         presenter.takeView(this)
 
-        setupStyle()
+        askList.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        askList.adapter = askAdapter
+        bidList.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        bidList.adapter = bidAdapter
+
+        currentStatusService.board.asObservable()
+            .throttleLast(1, TimeUnit.SECONDS)
+            .subscribeOnIoObserveOnUI()
+            .subscribeNext {
+                askAdapter.items.value = it.asks.sortedBy { it.price }.take(50)
+                bidAdapter.items.value = it.bids.sortedByDescending { it.price }.take(50)
+                askAdapter.notifyDataSetChanged()
+                bidAdapter.notifyDataSetChanged()
+            }.addTo(bag)
     }
 
     override fun onDetachedFromWindow() {
         presenter.dropView(this)
         super.onDetachedFromWindow()
-    }
-
-    private fun setupStyle() {
     }
 }
