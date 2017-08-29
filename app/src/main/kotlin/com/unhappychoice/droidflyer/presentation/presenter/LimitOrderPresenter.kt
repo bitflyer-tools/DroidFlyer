5e3bd174c46deff1ad851ebd83bb9172aa584e13
Yuji Ueki <unhappychoice@gmail.com>
2017-08-29 13:54:43 +0900

    :+1: Add grouping function to board

--- app/src/main/kotlin/com/unhappychoice/droidflyer/presentation/presenter/LimitOrderPresenter.kt
+++ app/src/main/kotlin/com/unhappychoice/droidflyer/presentation/presenter/LimitOrderPresenter.kt
@@ -1,14 +1,28 @@
 package com.unhappychoice.droidflyer.presentation.presenter
 
+import com.unhappychoice.droidflyer.extension.Variable
 import com.unhappychoice.droidflyer.presentation.view.LimitOrderView
 import io.reactivex.disposables.CompositeDisposable
 import mortar.ViewPresenter
 
 class LimitOrderPresenter : ViewPresenter<LimitOrderView>() {
+    val groupingSizes = listOf(1L, 10L, 50L, 100L, 250L, 500L, 1000L, 2500L, 5000L)
+    val groupingSize = Variable(1L)
+
     private val bag = CompositeDisposable()
 
     override fun onExitScope() {
         bag.dispose()
         super.onExitScope()
+    }
+
+    fun incrementGroupingSize() {
+        val index = groupingSizes.indexOf(groupingSize.value)
+        groupingSizes.getOrNull(index + 1)?.let { groupingSize.value = it }
+    }
+
+    fun decrementGroupingSize() {
+        val index = groupingSizes.indexOf(groupingSize.value)
+        groupingSizes.getOrNull(index - 1)?.let { groupingSize.value = it }
     }
 }
--- app/src/main/kotlin/com/unhappychoice/droidflyer/presentation/view/LimitOrderView.kt
+++ app/src/main/kotlin/com/unhappychoice/droidflyer/presentation/view/LimitOrderView.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import android.support.v7.widget.LinearLayoutManager
 import android.util.AttributeSet
 import com.github.salomonbrys.kodein.instance
+import com.jakewharton.rxbinding2.view.clicks
 import com.unhappychoice.droidflyer.domain.service.CurrentStatusService
 import com.unhappychoice.droidflyer.presentation.adapter.BoardAdapter
 import com.unhappychoice.droidflyer.presentation.adapter.BoardType
 import com.unhappychoice.droidflyer.presentation.presenter.LimitOrderPresenter
+import com.unhappychoice.droidflyer.presentation.style.DefaultStyle
 import com.unhappychoice.droidflyer.presentation.view.core.BaseView
 import com.unhappychoice.norimaki.extension.subscribeNext
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -27,14 +29,15 @@ class LimitOrderView(context: Context, attr: AttributeSet?) : BaseView(context, 
         super.onAttachedToWindow()
         presenter.takeView(this)
 
+        setupStyle()
+
         askList.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
         askList.adapter = askAdapter
         bidList.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
         bidList.adapter = bidAdapter
 
         currentStatusService.board.asObservable()
-            .throttleLast(1, TimeUnit.SECONDS)
-            .subscribeOn(Schedulers.computation())
+            .throttleLast(1, TimeUnit.SECONDS, Schedulers.computation())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeNext {
                 askAdapter.items.value = it.asks.sortedBy { it.price }
@@ -42,10 +45,31 @@ class LimitOrderView(context: Context, attr: AttributeSet?) : BaseView(context, 
                 askAdapter.notifyDataSetChanged()
                 bidAdapter.notifyDataSetChanged()
             }.addTo(bag)
+
+        presenter.groupingSize.asObservable()
+            .subscribeNext {
+                groupingTextView.text = it.toString()
+                askAdapter.groupSize.value = it
+                bidAdapter.groupSize.value = it
+                askAdapter.notifyDataSetChanged()
+                bidAdapter.notifyDataSetChanged()
+            }.addTo(bag)
+
+        plusButton.clicks().subscribeNext { presenter.incrementGroupingSize() }.addTo(bag)
+        minusButton.clicks().subscribeNext { presenter.decrementGroupingSize() }.addTo(bag)
     }
 
     override fun onDetachedFromWindow() {
         presenter.dropView(this)
         super.onDetachedFromWindow()
+    }
+
+    private fun setupStyle() {
+        groupingLabel.setTextColor(DefaultStyle.accentColor)
+        groupingTextView.setTextColor(DefaultStyle.accentColor)
+        plusButton.setTextColor(DefaultStyle.accentColor)
+        minusButton.setTextColor(DefaultStyle.accentColor)
+        plusButton.setBackgroundColor(DefaultStyle.darkerPrimaryColor)
+        minusButton.setBackgroundColor(DefaultStyle.darkerPrimaryColor)
     }
 }
