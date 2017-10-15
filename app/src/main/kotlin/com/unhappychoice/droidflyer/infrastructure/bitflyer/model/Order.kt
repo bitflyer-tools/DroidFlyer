b93b7c6c836cc7e66af5282a694831dac7aef7dc
Yuji Ueki <unhappychoice@gmail.com>
2017-10-16 00:57:26 +0900

    :bug: Fix calcuration order

--- app/src/main/kotlin/com/unhappychoice/droidflyer/infrastructure/bitflyer/model/Order.kt
+++ app/src/main/kotlin/com/unhappychoice/droidflyer/infrastructure/bitflyer/model/Order.kt
@@ -5,13 +5,13 @@ data class Order(val price: Long, val size: Double)
 fun List<Order>.floorBySize(size: Long) =
     fold(mutableMapOf<Long, Double>()) { map, order ->
         val price = (order.price / size) * size
-        map[price] = map[price] ?: 0.0 + order.size
+        map[price] = (map[price] ?: 0.0) + order.size
         map
     }.map { Order(it.key, it.value) }
 
 fun List<Order>.ceilBySize(size: Long) =
     fold(mutableMapOf<Long, Double>()) { map, order ->
         val price = ((order.price + size) / size) * size
-        map[price] = map[price] ?: 0.0 + order.size
+        map[price] = (map[price] ?: 0.0) + order.size
         map
     }.map { Order(it.key, it.value) }
