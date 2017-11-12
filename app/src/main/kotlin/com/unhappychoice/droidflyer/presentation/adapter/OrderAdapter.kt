acc7101cd24dfa2b9575ebd13d70cdec1a12d7b6
Yuji Ueki <unhappychoice@gmail.com>
2017-11-12 21:26:39 +0900

    Improve cell style

--- app/src/main/kotlin/com/unhappychoice/droidflyer/presentation/adapter/OrderAdapter.kt
+++ app/src/main/kotlin/com/unhappychoice/droidflyer/presentation/adapter/OrderAdapter.kt
@@ -9,19 +9,19 @@ import android.widget.TextView
 import com.unhappychoice.droidflyer.R
 import com.unhappychoice.droidflyer.extension.Variable
 import com.unhappychoice.droidflyer.infrastructure.bitflyer.model.ChildOrder
+import com.unhappychoice.droidflyer.presentation.style.DefaultStyle
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.subjects.PublishSubject
 
 class OrderAdapter(val context: Context?) : RecyclerView.Adapter<OrderAdapter.ViewHolder>() {
     val items = Variable<List<ChildOrder>>(listOf())
-    val clickItems = PublishSubject.create<ChildOrder>()
+    val clickCancel = PublishSubject.create<ChildOrder>()
 
     override fun getItemCount() = items.value.size
 
     override fun onBindViewHolder(holder: OrderAdapter.ViewHolder, position: Int) {
         val item = items.value[position]
         holder.bind(item)
-        holder.view.setOnClickListener { clickItems.onNext(item) }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): OrderAdapter.ViewHolder {
@@ -31,11 +31,29 @@ class OrderAdapter(val context: Context?) : RecyclerView.Adapter<OrderAdapter.Vi
 
     inner class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         fun bind(order: ChildOrder) {
-            priceTextView.text = order.price.toString()
-            sizeTextView.text = order.size.toString()
+            setupStyle(order)
+            typeTextView.text = if (order.side == "BUY") context?.getString(R.string.buy) else context?.getString(R.string.sell)
+            priceTextView.text = "${order.price} JPY"
+            sizeTextView.text = "${order.size} ɃFX"
+            cancelTextView.setOnClickListener { clickCancel.onNext(order) }
         }
 
+        private fun setupStyle(order: ChildOrder) {
+            if (order.side == "BUY") {
+                typeTextView.setBackgroundColor(DefaultStyle.buyColor)
+            } else {
+                typeTextView.setBackgroundColor(DefaultStyle.sellColor)
+            }
+
+            typeTextView.setTextColor(DefaultStyle.accentColor)
+            priceTextView.setTextColor(DefaultStyle.accentColor)
+            sizeTextView.setTextColor(DefaultStyle.accentColor)
+            cancelTextView.setTextColor(DefaultStyle.accentColor)
+        }
+
+        private val typeTextView = view.findViewById<TextView>(R.id.type)
         private val priceTextView = view.findViewById<TextView>(R.id.price)
         private val sizeTextView = view.findViewById<TextView>(R.id.size)
+        private val cancelTextView = view.findViewById<TextView>(R.id.cancel)
     }
 }
