51aea16d06cf1b3e21c8b1397634fed87de63af3
Yuji Ueki <unhappychoice@gmail.com>
2019-01-06 21:16:41 +0900

    Implement socket.io realtime API

--- app/src/main/kotlin/com/unhappychoice/droidflyer/infrastructure/bitflyer/RealtimeClient.kt
+++ app/src/main/kotlin/com/unhappychoice/droidflyer/infrastructure/bitflyer/RealtimeClient.kt
@@ -1,17 +1,17 @@
 package com.unhappychoice.droidflyer.infrastructure.bitflyer
 
+import android.util.Log
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import com.pubnub.api.PNConfiguration
-import com.pubnub.api.PubNub
-import com.pubnub.api.callbacks.SubscribeCallback
-import com.pubnub.api.models.consumer.PNStatus
-import com.pubnub.api.models.consumer.pubsub.PNMessageResult
-import com.pubnub.api.models.consumer.pubsub.PNPresenceEventResult
+import com.unhappychoice.droidflyer.extension.withLog
 import com.unhappychoice.droidflyer.infrastructure.bitflyer.model.Board
 import com.unhappychoice.droidflyer.infrastructure.bitflyer.model.Execution
 import com.unhappychoice.droidflyer.infrastructure.bitflyer.model.Ticker
 import io.reactivex.subjects.PublishSubject
+import io.socket.client.IO
+import io.socket.client.Socket
+import org.json.JSONArray
+import org.json.JSONObject
 
 /**
  * ```
@@ -29,8 +29,7 @@ class RealtimeClient(val gson: Gson) {
     val ticker: PublishSubject<Ticker> = PublishSubject.create<Ticker>()
     val executions: PublishSubject<List<Execution>> = PublishSubject.create<List<Execution>>()
 
-    private val configuration = PNConfiguration().apply { subscribeKey = "sub-c-52a9ab50-291b-11e5-baaa-0619f8945a4f" }
-    private val pubnub = PubNub(configuration)
+    private val host = "https://io.lightstream.bitflyer.com"
     private val channels = listOf(
         "lightning_board_FX_BTC_JPY",
         "lightning_board_snapshot_FX_BTC_JPY",
@@ -39,27 +38,41 @@ class RealtimeClient(val gson: Gson) {
     )
 
     init {
-        pubnub.addListener(object : SubscribeCallback() {
-            override fun status(pubnub: PubNub?, status: PNStatus?) {}
-            override fun message(pubnub: PubNub?, message: PNMessageResult?) {
-                publish(message)
-            }
+        val options = IO.Options().apply { transports = arrayOf("websocket") }
+        val socket = IO.socket(host, options)
 
-            override fun presence(pubnub: PubNub?, presence: PNPresenceEventResult?) {}
-        })
-        pubnub.subscribe().channels(channels).execute()
+        socket.on(Socket.EVENT_CONNECT) {
+            channels.forEach { channel -> socket.emit("subscribe", channel) }
+        }
+
+        channels.forEach { channel ->
+            socket.on(channel) { events ->
+                events
+                    .mapNotNull { it as? JSONObject }
+                    .forEach { publish(channel, it.toString(4)) }
+                events
+                    .mapNotNull { (it as? JSONArray) }
+                    .forEach { publish(channel, it.toString(4)) }
+            }
+        }
+
+        socket.on(Socket.EVENT_DISCONNECT) {
+            Log.d("Socket", "Disconnected")
+        }
+
+        socket.connect()
     }
 
-    private fun publish(message: PNMessageResult?) {
-        when (message?.channel) {
+    private fun publish(channel: String, body: String) {
+        when (channel) {
             "lightning_board_FX_BTC_JPY" ->
-                board.onNext(gson.fromJson(message.message, Board::class.java))
+                board.onNext(gson.fromJson(body, Board::class.java))
             "lightning_board_snapshot_FX_BTC_JPY" ->
-                boardSnapshot.onNext(gson.fromJson(message.message, Board::class.java))
+                boardSnapshot.onNext(gson.fromJson(body, Board::class.java))
             "lightning_ticker_FX_BTC_JPY" ->
-                ticker.onNext(gson.fromJson(message.message, Ticker::class.java))
+                ticker.onNext(gson.fromJson(body, Ticker::class.java))
             "lightning_executions_FX_BTC_JPY" ->
-                executions.onNext(gson.fromJson(message.message, object : TypeToken<List<Execution>>() {}.type))
+                executions.onNext(gson.fromJson(body, object : TypeToken<List<Execution>>() {}.type))
         }
     }
 }
